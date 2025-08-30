b910a09f7676d5fac513443c89aee0d945eccad4
Bappa Dhara <bdhara612@gmail.com>
2025-08-30 17:34:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -20,5 +22,3 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 Input.displayName = "Input"
 
 export { Input }
-
-    
