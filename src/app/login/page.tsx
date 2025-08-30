bc77db42327019c59e0b270869463b11bf07d648
Bappa Dhara <bdhara612@gmail.com>
2025-08-30 17:29:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 
-
 'use client';
 import * as React from 'react';
+import { useEffect } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
